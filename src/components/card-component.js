5ecb51652170555da9a3725adc18857c33a6d18f
oscar anillo <oscarani14@gmail.com>
2022-08-30 17:26:14 -0500

    add media query for desktop

--- src/components/card-component.js
+++ src/components/card-component.js
@@ -4,13 +4,17 @@ import axios from 'axios';
 export default function CardComponent(){
     const [data, setData] = useState([]);
 
-    useEffect(() => {
+    const clickHandler = () => {
         axios
         .get('https://api.adviceslip.com/advice')
         .then((res) => {
             setData(res.data)
         })
         .catch((err) => console.log(err));
+    }
+
+    useEffect(() => {
+        clickHandler();
     }, []);
 
     return (
@@ -19,13 +23,14 @@ export default function CardComponent(){
                {Object.values(data).map(item => (
                 <div key={item.id}>
                     <p>Advice #{item.id}</p>
-                    <h1>"{item.advice}"</h1>
+                    <h1>“{item.advice}”</h1>
                 </div>
                ))}
             </div>
-            <div>
-                <img src="/images/icon-dice.svg" alt="" />
-                <img src="/images/pattern-divider-mobile.svg" alt="" />
+            <img src="/images/pattern-divider-mobile.svg" alt="" className='mobile-pattern' />
+            <img src="/images/pattern-divider-desktop.svg" alt="" className='desktop-pattern' />
+            <div className='images'>
+                <img src="/images/icon-dice.svg" alt="" className='dice' onClick={clickHandler} />
             </div>
         </div>
     )
